43c5dad056e43b8bfa44f08e4b0b1e85d5307d91
Josue Patiño <josuesalvador093@gmail.com>
2021-02-25 19:42:44 -0600

    pruebas de array de mese

--- Activiada 4/Actividad 4.playground/Contents.swift
+++ Activiada 4/Actividad 4.playground/Contents.swift
@@ -6,22 +6,58 @@ for i in datos {
         print(i)
     }
 }
-5^2
+
 func Suma(uno:Float,  dos:Float)-> String{
     let valor = uno + dos;
     return "La suma de  \(uno) +  \(dos) es:  \(valor)"
 }
 
-enum meses:Int {
-    case enero = 1, febrero = 2, marzo=3, abril=4, mayo=5, junio=6, julio=7, agosto=8, septiembre=9, octubre = 10, noviembre = 11, diciembre = 12
+func potencia(base:Int, potencia:Int)->Int{
+    var base1 = potencia;
+    var total = base;
+    repeat {
+        total = total*base;
+        base1 = (base1-1)
+    } while (base1>1)
+    return total
 }
-func numeroMes (mes:Int){
-    meses.init(rawValue: mes)
-    
-    
-    //print(meses.init(rawValue: mes))
+
+enum meses:Int {
+    case enero, febrero, marzo, abril, mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre
+}
+func numeroMes (mes:String)->String{
+    return meses
+}
+
+let mes:meses = .marzo
+
+switch mes {
+case .enero:
+    print("Enero es el mes numero: 1")
+case .febrero:
+    print("Febrero es el mes numero: 2")
+case .marzo:
+    print("Marzo es el mes numero: 3")
+case .abril:
+    print("Abril es el mes numero: 4")
+case .mayo:
+    print("Mayo es el mes numero: 5")
+case .junio:
+    print("Junio es el mes numero: 6")
+case .julio:
+    print("Julio es el mes numero: 7")
+case .agosto:
+    print("Agosto es el mes numero: 8")
+case .septiembre:
+    print("Septiembre es el mes numero: 9")
+case .octubre:
+    print("Octubre es el mes numero: 10")
+case .noviembre:
+    print("Noviembre es el mes numero: 11")
+case .diciembre:
+    print("Diciembre es el mes numero: 12")
     
 }
 numeroMes(mes: 3)
 Suma(uno: 10.1, dos: 12.6)
-//potencia(base: 5, potencia: 2)
+potencia(base: 5, potencia: 2)
