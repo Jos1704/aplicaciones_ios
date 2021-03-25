91c9c511aca75bd0c9fadaa800b1b976deab9532
Josue Patiño <oruga1704@hotmail.com>
2021-03-25 00:43:26 -0400

    Actividad 8 base

---
+++ Actividad 8/Actividad 8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad 8
+//
+//  Created by user182849 on 3/25/21.
+//  Copyright © 2021 user182849. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
